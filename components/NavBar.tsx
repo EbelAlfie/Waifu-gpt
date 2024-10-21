7a7d5b7951748ecf3f036ec384d8aedab15cdf59
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-10-21 20:17:11 +0700

    added styles and main page

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+const NavProps = {
+}
+
+const NavBar = () => {
+  return (
+    <header className="nav_grad bg-gradient-to-b from-gray-600 px-5 py-8 text-start">
+    <nav>
+      <h2 className="text-white">NavBar</h2>
+    </nav>
+  </header>
+  )
+}
+
+export default NavBar
---
+++ models/ConstEnum.ts
@@ -0,0 +1,18 @@
+enum Elements {
+    Pyro,
+    Anemo,
+    Cryo,
+    Hydro,
+    Geo,
+    Dendro,
+}
+
+enum Nationality {
+    Mondstadt,
+    Liyue,
+    Inazuma,
+    Sumeru,
+    Fontaine,
+    Natlan,
+    Sneznaya
+}
