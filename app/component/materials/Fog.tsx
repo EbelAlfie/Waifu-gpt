c658685413cdf7f48df85800c88812953881bc5e
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-11-05 13:10:44 +0700

    adding logic to orbit cloud

--- app/component/materials/Fog.tsx
+++ app/component/materials/Fog.tsx
@@ -1,21 +1,33 @@
-import { Cloud } from "@react-three/drei"
-import { Color } from "@react-three/fiber"
-import { useRef } from "react"
-import { Group, MeshLambertMaterial, Object3DEventMap, REVISION, Spherical, Vector3 } from "three"
+"use client";
+
+import { CloudInstance, Clouds } from "@react-three/drei"
+import { Color, useFrame } from "@react-three/fiber"
+import { Group, MeshLambertMaterial, Object3DEventMap, REVISION, ShaderMaterial, Spherical, Vector3, WebGLProgramParametersWithUniforms } from "three"
+import texture from "@/assets/tex/cloud.png"
+import { ForwardedRef, forwardRef, useRef } from "react"
 
 class MistMaterial extends MeshLambertMaterial {
     constructor() {
         super()
           const opaque_fragment = parseInt(REVISION.replace(/\D+/g, '')) >= 154 ? 'opaque_fragment' : 'output_fragment'
           this.onBeforeCompile = (shader) => {
+            shader.uniforms.time = { value : 0.0 }
+            
             shader.vertexShader =
-              `attribute float cloudOpacity;
+              `uniform float time ;
+               attribute float cloudOpacity;
                varying float vOpacity;
               ` +
               shader.vertexShader.replace(
                 '#include <fog_vertex>',
                 `#include <fog_vertex>
                  vOpacity = cloudOpacity;
+                 float rad = length(position.xz);
+                 float angle = atan(position.x, position.z);
+                 float orbitX = rad * cos(angle + time/100.0) ;
+                 float orbitZ = rad * sin(angle + time/100.0) ;
+                 vec3 orbitPos = vec3(orbitX, position.y, orbitZ) ;
+                 gl_Position = vec4(orbitPos, 1.0) ;
                 `
               )
             shader.fragmentShader =
@@ -27,6 +39,8 @@ class MistMaterial extends MeshLambertMaterial {
                  gl_FragColor = vec4(outgoingLight, diffuseColor.a * vOpacity);
                 `
               )
+            
+            this.userData = { "shader": shader }
           }
     }
 }
@@ -35,25 +49,45 @@ type CloudProp = {
     color: Color | string | number
 }
 
-const CharacterFog = (props: CloudProp) => {
+const CharacterFog = (props: CloudProp)=> {
+
+  const cloudRef = useRef<Group<Object3DEventMap>>(null)
+  const matRef = useRef<MistMaterial>(null)
   
   const distribute = () => {
     return {
-      point: new Vector3().setFromSpherical(new Spherical(10, Math.acos(1 - Math.random() * 2), Math.random() * 2 * Math.PI))
+      point: new Vector3().setFromSpherical(new Spherical(10, Math.acos(1 - Math.random() * 2), Math.random() * 2 * Math.PI)),
     }
   }
 
+  useFrame((state) => {
+    console.log(cloudRef.current)
+    return cloudRef.current
+  })
+
+  // useFrame(state => 
+  //   matRef.current &&
+  //   (matRef.current.userData = state.clock.getElapsedTime())
+  // )
+
   return (
-      <mesh >
-          <Cloud 
+      <mesh
+      >
+        <Clouds 
+          texture={texture.src}
+          material={MistMaterial}
+        >
+          <CloudInstance
+              ref={cloudRef}
               bounds={[1, 1, 1]}
               color={props.color}
               distribute={distribute}
               concentrate = "inside"
-              volume={20}
-              scale={10.0}
+              volume={30}
+              scale={5.0}
               growth={10}
           />
+        </Clouds>
       </mesh>
     )
 }
