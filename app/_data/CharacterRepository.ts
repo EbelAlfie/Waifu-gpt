5f812f6915596f859f7410142cd32b409f5241e9
Davis Inde Satya <davis.onde.wei@gmail.com>
2025-04-23 13:17:14 +0700

    move api call

--- app/_data/CharacterRepository.ts
+++ app/_data/CharacterRepository.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios"
-import { CharacterResponse } from "./model/Character"
-import { BaseResponse } from "./model/BaseResponse"
+import { CharacterResponse } from "../../api/data/model/Character"
+import { BaseResponse } from "../../api/data/model/BaseResponse"
 
 export class CharacterRepository {
     async getCharacterList() {
@@ -16,8 +16,6 @@ export class CharacterRepository {
             data: request,
             headers: {
                 "x-rpc-language": "en-us",
-                'Content-Type': 'application/json', 
-                'Cookie': 'ltmid_v2=1wfkk1cxcp_hy; ltoken_v2=v2_CAISDGM5b3FhcTNzM2d1OBokYjVmMDdiNmYtMGNhMS00MGMyLTllZTItMTBkZDBjMTFlNzRiIMyinsAGKMH2j8YFMOSF24wBQgtiYnNfb3ZlcnNlYQ.TJEHaAAAAAAB.MEQCIEhABYfRFHKot802VNLOgKQ0a7h3QpP8HbEfmJscV61pAiBR7nnbgmTzaFlC4WLxuvTOEX9nrD3D_LwGWywv41fFRw; ltuid_v2=295092964'
             },
         }
         return axios.request(config)
