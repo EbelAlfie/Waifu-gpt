f85083b1f1ba9b6bdc9d2e0a72288eda549588ce
Davis Inde Satya <davis.onde.wei@gmail.com>
2025-04-26 22:38:05 +0700

    fix borders

--- app/component/character/CharaAvatar.tsx
+++ app/component/character/CharaAvatar.tsx
@@ -1,9 +1,11 @@
 export const CharacterIcon = ({icon}: {icon: string}) => {
     return <>
+    <div className="relative shrink-0">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-blue-900 border-2 size-20"/>
         <img 
-            className="rounded-full size-24 -translate-y-3"
+            className="relative size-24 -translate-y-3"
             src={icon}
         />
-        
+    </div>
     </>
 }
--- app/component/character/CharacterSlider.tsx
+++ app/component/character/CharacterSlider.tsx
@@ -3,11 +3,11 @@ import { CharacterIcon } from "./CharaAvatar"
 
 export const CharacterSlider = ({list}: {list: CharacterModel[]}) => {
     const characters = list.map((chara) => {
-        return <CharacterIcon icon={chara.sideIcon}/>
+        return <CharacterIcon key={chara.id} icon={chara.sideIcon}/>
     })
 
     return (
-        <div className="flex flex-row items-center w-screen overflow-x-scroll scroll-none">
+        <div className="flex items-center w-screen overflow-x-scroll scroll-none">
             {characters}
         </div>
     )
